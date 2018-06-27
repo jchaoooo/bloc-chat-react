3ae39b3bf1b22009e78154af764e686b5c10e272
Jessica Chao <j.chao63@gmail.com>
2018-06-26 20:56:05 -0700

    Checkpoint 6 Send Messages: Revised Timestamp

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -55,7 +55,23 @@ class MessageList extends Component {
   handleMessageSubmit(e) {
     e.preventDefault();
     this.createMessage();
-    this.setState({ content: "" });
+    this.setState({
+      content: "",
+      sentAt: this.formatTime()
+     });
+  }
+
+  formatTime(time) {
+    const date = new Date(time);
+    const year = date.getFullYear();
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const month = months[date.getMonth()];
+    const day = date.getDate();
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+    const timestamp = month + ' ' + day + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+    return timestamp;
   }
 
   render() {
@@ -68,7 +84,7 @@ class MessageList extends Component {
             <div key={index} className="messages">
               <p id="username">Username: {message.username}</p>
               <p id="content">Message: {message.content}</p>
-              <p id="timestamp">Timestamp: {message.sentAt}</p>
+              <p id="timestamp">Timestamp: {this.formatTime(message.sentAt)}</p>
             </div>
             )}
         </section>
